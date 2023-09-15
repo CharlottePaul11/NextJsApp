724d94d017d458640936fe399ffe6468727648b3
Charlotte Paul <charlotte.paul@mac6184.home>
2023-09-15 15:02:20 +0100

    changes to calc

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,6 +1,5 @@
+import { Html } from 'next/document';
 import Link from 'next/link';
-import '/styles/app.css';
-
 export default function FirstPost() {
   return (
     <>
@@ -8,12 +7,10 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home page</Link>
       </h2>
-
       <div className='container'>
           <form>
               <input type="text"/>
           </form>
-
           <div className="keypad">
             <button>Clear</button>
             <button>C</button>
@@ -34,6 +31,35 @@ export default function FirstPost() {
             <button>.</button>
             <button>=</button>
           </div>
+          <style jsx global>{`
+  body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+* {
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 18px;
+}
+.container {
+    top: 50%;
+    left: 50%;
+    position: absolute;
+    transform: translate(-50%, -50%);
+    margin: 0 auto;
+    width: 260px;
+    text-align: center;
+    border: 5px solid black;
+    background: grey;
+    border-radius: 5px;
+}
+.keypad {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-auto-flow: minmax(60px, auto)
+}
+`}
+</style>
       </div>
     </>
   );
