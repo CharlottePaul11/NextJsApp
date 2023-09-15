c178b8ebb0bbd2a3969048f6ca4630db1a95c03f
Charlotte Paul <charlotte.paul@mac6184.home>
2023-09-15 14:38:44 +0100

    changes to calc

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import '/styles/app.css';
 
 export default function FirstPost() {
   return (
@@ -7,6 +8,33 @@ export default function FirstPost() {
       <h2>
         <Link href="/">Back to home page</Link>
       </h2>
+
+      <div className='container'>
+          <form>
+              <input type="text"/>
+          </form>
+
+          <div className="keypad">
+            <button>Clear</button>
+            <button>C</button>
+            <button>&divide;</button>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>&times;</button>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>&ndash;</button>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>+</button>
+            <button>0</button>
+            <button>.</button>
+            <button>=</button>
+          </div>
+      </div>
     </>
   );
 }
